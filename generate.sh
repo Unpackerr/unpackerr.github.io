525f0173fb8eca8e53bf9ce01c49a2d5fe09f838
David Newhall II <david@sleepers.pro>
2026-03-03 18:33:29 -0800

    provide workflow dispatch button

--- generate.sh
+++ generate.sh
@@ -1,8 +1,10 @@
 # The beginning: https://github.com/Unpackerr/unpackerr/pull/459
 # The generator: https://github.com/Unpackerr/unpackerr/tree/main/init/config
 
+UNPACKERR_BRANCH=${UNPACKERR_BRANCH:-main}
+
 # We do this because go cache will download an older version.
 go env -w 'GOPRIVATE=github.com/Unpackerr/*'
 
 # Run the config generator directly from github.
-go run github.com/Unpackerr/unpackerr/init/config@main --type docusaurus --output docs/install/generated
+go run github.com/Unpackerr/unpackerr/init/config@${UNPACKERR_BRANCH} --type docusaurus --output docs/install/generated
